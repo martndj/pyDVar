1ad56d4cf5c69279cbdc16b9427999280ba6b820
Martin Deshaies-Jacques <martn@martn.info>
2013-08-30 13:13:21 -0400

    Coming back to ad1Try... still not converging...

--- ad1Try.py
+++ ad1Try.py
@@ -17,15 +17,29 @@ x_bkg=kdv.soliton(g.x, -4.2, 1.1, 0.3, 1.2, -1.76)
 
 #----| Observations |---------
 idxObs=[30,35,40,41,49,50,55,57,60,78,89,90]
-H=opObs_exactIdx(g, idxObs)
+H=opObs_exactIdx(g.N, idxObs)
 obs=np.dot(H,x_truth)
 
 
 #----| Preconditionning |-----
+Lc=5.
+sig=0.2
+corr=fCorr_isoHomo(g.x, Lc)
+rCTilde=rCTilde_sqrt_isoHomo(g, corr)
+var=sig*np.ones(g.N)
+
 xi=np.zeros(g.N)
-corr=fCorr_isoHomo(g, 5.)
-rCTilde=spCorr_isoHomo(corr)
-var=0.2*np.ones(g.N)
+
+#----| testing B_sqrt |-------
+xDirac=np.zeros(g.N)
+xDirac[g.N/2]=1.
+xiTest=B_sqrt_op_T(xDirac, g.N,  var, rCTilde)
+xTest=B_sqrt_op(xiTest, g.N, var, rCTilde)
+plt.figure()
+plt.plot(g.x, xTest/sig**2)
+plt.plot(g.x, corr)
+plt.show()
+
 
 #----| Departure |------------
 Hx=np.dot(H, x_bkg)
@@ -33,13 +47,14 @@ d=Hx-obs
 R_inv=np.eye(len(idxObs))
 
 #----| Minimizing |-----------
-args=(d, R_inv, B_sqrt_op, H, g.N, var, rCTilde)
+args=(d, R_inv, B_sqrt_op_T, H, g.N, var, rCTilde)
 xi_a, out=sciOpt.fmin_bfgs(costFunc, xi, fprime=gradCostFunc,  
                 args=args, retall=True, maxiter=1000)
 
-x_a=x_bkg+B_sqrt_op(xi_a, N, var, rCTilde)
+x_a=x_bkg+B_sqrt_op(xi_a, g.N, var, rCTilde)
 
 
+plt.figure()
 plt.plot(g.x, x_truth)
 plt.plot(g.x, x_bkg)
 #plt.plot(g.x, signal)
