71c7ed821f6efa82f754edfe3b80f72c7c4d9a41
Martin Deshaies-Jacques <martn@martn.info>
2013-09-03 15:29:54 -0400

    The correlation retrieval seems to work
    as in Pierre's:
        scilabPierre/sca7212_1Dvar_Spectral.sce

--- B_sqrtValid.py
+++ B_sqrtValid.py
@@ -1,19 +1,19 @@
 import numpy as np
+import random as rnd
 from dataAssLib import *
 import matplotlib.pyplot as plt
 import pyKdV as kdv
 
 Ng=100
-sig=1.
 # pour sig=0.1 ca colle presque...
 g=kdv.SpectralGrid(Ng, 100., aliasing=1)
 
-xDirac=np.zeros(g.N)
-xDirac[Ng/2]=1.
 
+sig=1.
+lCorr=5.
 variances=sig*np.ones(g.N)
-fCorr=fCorr_isoHomo(g.x, 5.)
-rFTilde=c2r(np.fft.fft(fCorr))
+fCorr=fCorr_isoHomo(g.x, lCorr)
+rFTilde=g.N*c2r(np.fft.fft(fCorr))
 
 # Attention, C est une matrice, un tenseur d'ordre 2,
 # mais on l'ecrit comme un vecteur
@@ -30,9 +30,32 @@ for i in xrange(1, (g.N-1)/2+1):
 
 CTilde_sqrt=np.sqrt(rCTilde)
 
-
+# correlation test
+xDirac=np.zeros(g.N)
+xDirac[Ng/4]=1.
 xiTest=B_sqrt_op_T(xDirac, g.N,  variances, CTilde_sqrt)
 xTest=B_sqrt_op(xiTest, g.N, variances, CTilde_sqrt)
-plt.plot(g.x, xTest/sig**2)
+
+# adjoint test
+rnd.seed(0.4573216806)
+mu=0.; sigNoise=2.
+xNoise=np.zeros(g.N)
+yNoise=np.zeros(g.N)
+for i in xrange(g.N):
+    yNoise[i]=rnd.gauss(mu, sigNoise)
+    xNoise[i]=rnd.gauss(mu, sigNoise)
+testDirect=np.dot(xNoise,
+                    B_sqrt_op(yNoise, g.N, variances, CTilde_sqrt).conj())
+testAdjoint=np.dot(B_sqrt_op_T(xNoise, g.N, variances, CTilde_sqrt),
+                    yNoise.conj())
+
+#print(testDirect)
+#print(testAdjoint)
+print("Adjoint test with noise: <x,Gy>-<G*x,y>")
+print(testDirect-testAdjoint)
+
+
+#plt.plot(g.x, xTest/sig**2)
+plt.plot(g.x, xTest)
 plt.plot(g.x, fCorr)
 plt.show()
