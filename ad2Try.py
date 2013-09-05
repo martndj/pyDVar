d85ce7bf0b562c8fb7ed785d2ea1dffbe13e4439
Martin Deshaies-Jacques <martn@martn.info>
2013-09-05 16:46:58 -0400

    Found the bug: I'll merge with master

--- ad2Try.py
+++ ad2Try.py
@@ -23,7 +23,7 @@ for i in xrange(nObs):
     idxObs[i]=np.min(np.where(g.x>=posObs[i]))
 H=opObs_exactIdx_op
 H_T=opObs_exactIdx_op_T
-argsH=(g, idxObs,)
+argsH=(g, idxObs)
 
 obs=H(x_truth, *argsH)
 sigR=1.
@@ -37,9 +37,13 @@ rCTilde_sqrt=rCTilde_sqrt_isoHomo(g, corr)
 var=sig*np.ones(g.N)
 xi=np.zeros(g.N)
 
-#----| Minimizing |-----------
-args=(x_bkg, g.N, var, B_sqrt_op, B_sqrt_op_T, 
+
+
+args=(x_bkg, var, B_sqrt_op, B_sqrt_op_T, 
         H, H_T, argsH, obs, R_inv, rCTilde_sqrt)
+#----| Gradient test |--------
+resultGradTest=gradTest(costFunc, gradCostFunc, xi, *args)
+#----| Minimizing |-----------
 xi_a, out=sciOpt.fmin_bfgs(costFunc, xi, fprime=gradCostFunc,  
                 args=args, retall=True, maxiter=100)
 
@@ -48,13 +52,13 @@ resultGradTest=gradTest(costFunc, gradCostFunc, xi_a, *args)
 
 
 #----| Analysis |-------------
-x_a=B_sqrt_op(xi_a, g.N, var, rCTilde_sqrt)+x_bkg
+x_a=B_sqrt_op(xi_a, var, rCTilde_sqrt)+x_bkg
 
 plt.figure()
 plt.plot(g.x, x_truth,'k--')
 plt.plot(g.x, x_bkg,'m')
 plt.plot(g.x[idxObs], obs, 'go')
-plt.plot(g.x[idxObs], H(x_bkg, argsH), 'mo')
+plt.plot(g.x[idxObs], H(x_bkg, *argsH), 'mo')
 plt.plot(g.x, x_a, 'r')
 plt.plot(g.x[idxObs], x_a[idxObs],'ro')
 plt.legend(['$x_t$', '$x_b$', '$y$', r'$H(x)$', r'$x_a$'])
